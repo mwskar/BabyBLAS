b0dd466de11ce5fb6e3d4534a63ef18e2b3b222f
Michael W. Skarbek <skarbek_mw@hammer.cs.mercer.edu>
2022-04-19 23:58:14 -0400

    Forgot the driversd

--- driver.f90
+++ driver.f90
@@ -1,22 +1,55 @@
-program tester
+program driver 
+
+integer :: NDIM
 
 real (kind=8) :: wall_start, wall_end
 real (kind=8) :: cpu_start, cpu_end
-real (kind=8) :: trace, mflops
+real (kind=8) :: trace
 
 
-integer :: problem_size, nthreads
+integer :: startval, stopval, stepval, nthreads
 real (kind=8) :: walltime
 real (kind=8) :: cputime 
 external walltime, cputime
 
-character (len=8) :: carg1, carg2, carg3
+character (len=8) :: carg1, carg2, carg3, carg4
 
 real (kind=8), dimension(:), allocatable :: veca, vecb
 real (kind=8), dimension(:,:), allocatable :: matrixa, matrixb, matrixc
 
-external :: dot
-double precision :: dot
+#ifdef ACCURACY_TEST
+
+!This portion of code is ONLY used for verifying the accuracy of the code using
+!the matrix and matrix inverse stored on the class website.
+
+!Download the files from theochem using curl (don't store these on anvil!)
+call system("curl -s -o matrixa.dat --url http://theochem.mercer.edu/csc435/data/matrixa.dat")
+call system("curl -s -o matrixb.dat --url http://theochem.mercer.edu/csc435/data/matrixb.dat")
+
+NDIM = 100  ! The test files are 100x100 double precision matrix and its inverse
+nthreads = 2
+allocate ( matrixa(NDIM,NDIM), stat=ierr)
+allocate ( matrixb(NDIM,NDIM), stat=ierr)
+allocate ( matrixc(NDIM,NDIM), stat=ierr)
+open (unit=5,file="matrixa.dat",status="old")
+do i = 1, NDIM
+  do j = 1, NDIM
+     read(5,*) matrixa(j,i)
+  enddo
+enddo
+close(5)
+open (unit=5,file="matrixb.dat",status="old")
+do i = 1, NDIM
+  do j = 1, NDIM
+     read(5,*) matrixb(j,i)
+  enddo
+enddo
+close(5)
+
+! Delete the files from disk
+call system("rm matrixa.dat matrixb.dat")
+
+#else
 
 ! Start the normal processing here.  Read the starting, stop, and step values
 ! as well as the number of threads to use.
@@ -25,132 +58,78 @@ double precision :: dot
 call get_command_argument(1, carg1)
 call get_command_argument(2, carg2)
 call get_command_argument(3, carg3)
+call get_command_argument(4, carg4)
 
 ! Use Fortran internal files to convert command line arguments to ints
 
-read (carg1,'(i8)') problem_size
-read (carg2,'(i8)') nthreads 
+read (carg1,'(i8)') startval
+read (carg2,'(i8)') stopval
+read (carg3,'(i8)') stepval
+read (carg4,'(i8)') nthreads 
 
+! Start the outermost loop to run tests of varying matrix size
+ 
+do iter = startval, stopval, stepval
 
+NDIM = iter
 
+allocate ( veca(NDIM), stat=ierr)
+allocate ( vecb(NDIM), stat=ierr)
+allocate ( matrixa(NDIM,NDIM), stat=ierr)
+allocate ( matrixb(NDIM,NDIM), stat=ierr)
+allocate ( matrixc(NDIM,NDIM), stat=ierr)
 
+! Build veca and vecb which, their tensor product creates the two matrices 
+! to be multiplied.
 
-allocate(veca(problem_size))
-allocate(vecb(problem_size))
+do i = 1, NDIM 
+     veca(i) = 1.0
+     vecb(i) = 1.0 / sqrt( dble(NDIM))
+enddo
 
-
-do i=1, problem_size
-  !veca(i) = dble(problem_size)/DSQRT(dble(i))
-  !vecb(i) = (2.0D0*dble(problem_size))/DSQRT(dble(i))
-  veca(i) = 1.0
-  vecb(i) = 1.0 / SQRT(dble(problem_size))
-end do
-
-
-
-
-
-
-
-if (carg3 == "DOT") then
-
-
-wall_start = walltime()
-trace = dot(nthreads, problem_size, veca, vecb)
-wall_end = walltime()
-
-mflops = (2.0D0 * dble(problem_size) ) / ( (wall_end-wall_start) * 1.0e6)
-
-print *, "Trace: ", trace, " mflops: ", mflops
-
-
-
-
-else if (carg3 == "VVM") then
-
-allocate(matrixc(problem_size,problem_size))
-matrixc = 0.0D0
-
-wall_start = walltime()
-call vvm(nthreads, problem_size, veca, vecb, matrixc)
-wall_end = walltime()
-
-mflops = dble(problem_size)**2.0D0 / ( (wall_end-wall_start) * 1.0e6 )
-
-trace = 0.0D0
-do i=1, problem_size
-  trace = trace + matrixc(i,i)
-end do
-
-print *, "Trace: ", trace, " mflops: ", mflops
-
-deallocate(matrixc)
-
-
-
-
-else if ( carg3 == "MVV") then
-
-allocate(matrixc(problem_size,problem_size))
-matrixc = 0.0D0
-
-call vvm(nthreads, problem_size, veca, vecb, matrixc)
-
-
-wall_start = walltime()
-call mvv(nthreads, problem_size, veca, vecb, matrixc)
-wall_end = walltime()
-
-do i=1, problem_size
-  trace = trace + vecb(i)
-end do
-
-mflops = (2.0D0 * dble(problem_size)**2 ) / ( (wall_end-wall_start) * 1.0e6)
-
-print *, "Trace: ", trace, " mflops: ", mflops
-
-deallocate(matrixc)
-
-else if (carg3 == "MMM") then
-
-
-allocate(matrixa(problem_size, problem_size))
-allocate(matrixb(problem_size, problem_size))
-allocate(matrixc(problem_size,problem_size))
-
-
+! Zero the matrices using Fortran 90 syntax.
 matrixa = 0.0
 matrixb = 0.0
-matrixc = 0.0D0
 
-call vvm(nthreads, problem_size, veca, vecb, matrixa)
-call vvm(nthreads, problem_size, veca, vecb, matrixb)
+call vvm(NDIM, veca, vecb, matrixa)
+call vvm(NDIM, veca, vecb, matrixb)
+
+#endif
 
 wall_start = walltime()
+cpu_start = cputime()
 
-call mmm(nthreads, problem_size, matrixa, matrixb, matrixc)
+call mmm(nthreads, NDIM, matrixa, matrixb, matrixc)
 
+cpu_end = cputime()
 wall_end = walltime()
 
-trace = 0.0D0
-do i=1, problem_size
-  trace = trace + matrixc(i,i)
-end do
+trace = 0.0;
 
-mflops = ( 2.0D0 * dble(problem_size)**3.0D0 ) / ( (wall_end-wall_start) * 1.0e6 )
+do i=1, NDIM 
+     trace = trace + matrixc(i,i)
+enddo
 
-print *, "Trace: ", trace, " mflops: ", mflops
+! Calculate megaflops based on CPU time and Walltime
+
+mflops  = 2*dble(NDIM)**3/ (cpu_end-cpu_start) / 1.0e6
+mflops2 = 2*dble(NDIM)**3/ (wall_end-wall_start)/ 1.0e6
+ 
+print *, NDIM, trace, cpu_end-cpu_start, wall_end-wall_start,  mflops, mflops2
+
+
+! Free the memory that was allocated based on which version of the program was
+! run.
 
 deallocate(matrixa)
 deallocate(matrixb)
 deallocate(matrixc)
-
-end if
-
-
+#ifndef ACCURACY_TEST
 deallocate(veca)
 deallocate(vecb)
 
+enddo
+#endif
 
-
-end program tester
+end program driver 
+ 
